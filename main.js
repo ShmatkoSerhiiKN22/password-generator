a10779b959d7315b59545a65e46718fc4b9ecd5b
serhii shamtko <shmatko_ak23@nuwm.edu.ua>
2024-05-11 17:09:36 +0300

    feat(app): add input for select length

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 import './style.css'
 
+const changeHandler = (id) => (event) => {
+  document.getElementById(id).value = event.target.value
+}
+
 document.querySelector('#app').innerHTML = `
   <div>
-    <h1>Hello World!</h1>
+    <h1>Генератор паролів</h1>
+    <p>Оберіть довжину пароля</p>
+    <div class="flex">
+      <input type="range" id="length" min="0" max="20" />
+      <input id="length-value" type="number" min="0" max="20" value="10" />
+    </div>
   </div>
 `
+
+document.getElementById('length').addEventListener('input', changeHandler('length-value'))
+document.getElementById('length-value').addEventListener('input', changeHandler('length'))
